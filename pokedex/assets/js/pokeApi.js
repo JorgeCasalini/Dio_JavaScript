b5a108a35ab52da773aa5dfbda17533effdb97ec
jorgeCasalini <joshcasalini@gmail.com>
2024-02-07 22:30:11 -0300

    separate api from the rest of the main.js body, importing the code and facilitating future updates

--- pokedex/assets/js/pokeApi.js
+++ pokedex/assets/js/pokeApi.js
@@ -1,19 +1,17 @@
 const PokeApi = {}
 
+PokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+
 PokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-}
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-    fetch('https://pokeapi.co/api/v2/pokemon/5'),
-    fetch('https://pokeapi.co/api/v2/pokemon/6'),
-    fetch('https://pokeapi.co/api/v2/pokemon/7')
-]).then((results) => {
-    console.log(results)
-})
+        .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}       
